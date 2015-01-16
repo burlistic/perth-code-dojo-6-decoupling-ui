74691c93cc95acfc2cddf63e6c9c035a67a81185
burlistic <will.webster@mykolab.com>
2015-01-16 15:29:00 +0800

    Added first test - WIP. Fixed up existing test which I broke.

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -25,11 +25,48 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .factory('timeSlotService', ['$window', 'currentTimeService', function(win, currentTimeService) {
-  var timeSlotServiceInstance;
+  var timeSlotServiceInstance = {
+    slots : []
+  }
+
+  // constructor?
 
   // factory function body that constructs shinyNewServiceInstance
 
-  win.console.log(currentTimeService);
+  //win.console.log(currentTimeService);
+
+  var time = {
+    hour : currentTimeService.getHours(),
+    minute : currentTimeService.getMinutes()
+  };
+
+  var endOfTheDay = new Date();
+  endOfTheDay.setHours(17);
+  endOfTheDay.setMinutes(30);
+  endOfTheDay.setSeconds(0);
+  endOfTheDay.setMilliseconds(0);
+
+  var startOfTheDay = new Date();
+  startOfTheDay.setHours(7);
+  startOfTheDay.setMinutes(0);
+  startOfTheDay.setSeconds(0);
+  startOfTheDay.setMilliseconds(0);
+
+  var ctr = 0;
+  while(new Date(endOfTheDay.getFullYear(), endOfTheDay.getMonth(), endOfTheDay.getDate(), time.hour, time.minute, 0, 0) <= endOfTheDay)
+  {
+      timeSlotServiceInstance.slots[ctr] = time;
+
+      // move into seperate method... service ?
+      if(time.minute === 30){
+        time.minute = 0;
+        time.hour++;
+      } else {
+        time.minute = 30;
+      }
+
+      ctr++;
+  }
 
   return timeSlotServiceInstance;
 }])
--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -8,9 +8,31 @@ describe('myApp.view1 module', function() {
 
     it('should ....', inject(function($controller) {
       //spec body
-      var view1Ctrl = $controller('View1Ctrl');
+      var $scope = {};
+
+      var view1Ctrl = $controller('View1Ctrl', { $scope: $scope }); 
       expect(view1Ctrl).toBeDefined();
     }));
 
   });
+
+ //  describe('view1 timeSlotService', function(){
+
+ //  	var timeSlotService;
+
+ //  	beforeEach(function() {
+
+	//   inject(function($injector) {
+	//     timeSlotService = $injector.get('timeSlotService');
+	//   });
+	// });
+
+
+ //    it('should populate 34 half hour slots for a full day', inject(function($service) {
+ //      //spec body
+ //      expect(timeSlotService.slots.length).toEqual(12);
+ //    }));
+
+ //  });
+
 });
