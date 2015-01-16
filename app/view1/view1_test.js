906a5e894fa85f185a2a3cf0c443f198f1b8bfc6
burlistic <will.webster@mykolab.com>
2015-01-16 16:01:42 +0800

    Have mock injection working in test - still red

--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -18,14 +18,21 @@ describe('myApp.view1 module', function() {
 
   describe('view1 timeSlotService', function(){
 
-  	var timeSlotService;
+  	var timeSlotService, mockCurrentTimeService
 
   	beforeEach(function() {
 
-	  inject(function($injector) {
-	    timeSlotService = $injector.get('timeSlotService');
-	  });
-	});
+      mockCurrentTimeService = new Date(2014, 8, 4, 6, 0, 0);
+
+      module(function ($provide) {
+          $provide.value('currentTimeService', mockCurrentTimeService);
+      });
+
+  	  // inject(function($injector) {
+  	  //   timeSlotService = $injector.get('timeSlotService');
+  	  // });
+	   
+    });
 
 
     it('should populate 34 half hour slots for a full day', inject(function(timeSlotService) {
